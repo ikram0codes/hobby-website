5be116ea9745a1f8ee66e95350d9730577754b9e
IKRAM <ikram0pakitan@gmail.com>
2024-06-17 21:24:20 +0500

    anothercommit

---
+++ index.js
@@ -0,0 +1,365 @@
+// * {
+//     margin: 0px;
+//     padding: 0px;
+//     box-sizing: border-box;
+//     font-family: Poppins, sans-serif;
+// }
+
+// html,
+// body {
+//     width: 100vw;
+//     height: 100vmax;
+//     background-color: white;
+//     color: black;
+//     scroll-behavior: smooth;
+//     overflow-x: hidden;
+// }
+
+// a {
+//     color: inherit;
+//     text-decoration: none;
+// }
+
+// header {
+//     width: 100%;
+//     height: 100vh;
+//     background-color: #040E27;
+//     padding: 16px;
+//     overflow: hidden;
+// }
+
+// main {
+//     height: max-content;
+// }
+
+// button {
+//     cursor: pointer;
+// }
+
+// .navbar {
+//     display: grid;
+//     grid-template-columns: 33% 40% auto;
+// }
+
+// .navbar h1 {
+//     font-family: Lucida handwriting;
+//     color: gold;
+//     font-size: 40px;
+// }
+
+// .navbar ul {
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     gap: 25px;
+
+// }
+
+// .navbar ul li {
+//     list-style: none;
+// }
+
+// .navbar ul li a {
+//     text-decoration: none;
+//     color: white;
+//     font-size: 17px;
+//     text-transform: uppercase;
+//     font-family: Poppins, sans-serif;
+// }
+
+// .cta {
+//     width: 100%;
+//     height: 100%;
+//     display: flex;
+//     justify-content: flex-end;
+//     align-items: center;
+// }
+
+// .cta button {
+//     width: 7rem;
+//     height: 2.8rem;
+//     background-color: white;
+//     border: none;
+//     outline: none;
+//     font-size: 18px;
+//     border-radius: 50px;
+//     color: black;
+
+// }
+
+// .hero {
+//     width: 100%;
+//     height: 100%;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     flex-direction: column;
+//     gap: 35px;
+// }
+
+// .hook h1 {
+//     font-size: 50px;
+//     color: white;
+//     text-align: center;
+// }
+
+// .hook p {
+//     font-size: 20px;
+//     text-align: center;
+//     color: #ccc;
+//     font-weight: 300;
+// }
+
+// .hook span {
+//     font-family: Lucida handwriting;
+//     color: gold
+// }
+
+// .btns {
+//     width: max-content;
+
+// }
+
+// .btns button {
+//     width: 7rem;
+//     height: 2.8rem;
+//     border: 2px solid white;
+//     background-color: #040E27;
+//     color: white;
+//     outline: none;
+//     font-size: 16px;
+//     border-radius: 50px;
+//     margin: 10px 20px;
+//     transition: 300ms ease-in-out;
+// }
+
+// .btns button:hover {
+//     background-color: gold;
+//     color: black;
+//     border: none;
+// }
+
+// #about-me {
+//     width: 100vw;
+//     height: 100vh;
+//     padding: 16px;
+//     display: flex;
+//     justify-content: flex-start;
+//     align-items: center;
+//     flex-direction: column;
+//     gap: 20px;
+// }
+
+// .head {
+//     text-align: center;
+//     font-size: 40px;
+//     font-weight: 800;
+
+//     font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+//     border-bottom: 5px solid #040E27;
+// }
+
+// #about-me div {
+//     width: 100%;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     flex-direction: row-reverse;
+//     padding: 20px;
+//     gap: 20px;
+// }
+
+// #about-me div img {
+//     width: 350px;
+//     height: max-content;
+//     object-fit: cover;
+//     object-position: center;
+//     border-radius: 10px;
+// }
+
+// #about-me div p {
+//     width: 700px;
+//     text-align: center;
+// }
+
+// #about-me div p a {
+//     text-decoration: underline;
+//     color: #040E27;
+
+// }
+
+// footer {
+//     width: 100%;
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 16px;
+//     background-color: #040E27;
+// }
+
+// footer p {
+//     font-size: 20px;
+//     color: white;
+// }
+
+// #hobby {
+//     padding: 16px;
+//     width: 100vw;
+//     height: max-content;
+//     display: flex;
+//     justify-content: flex-start;
+//     align-items: center;
+//     flex-direction: column;
+//     gap: 20px;
+//     background-color: #040E27;
+// }
+
+// .hob-desc {
+//     width: 100%;
+//     height: 100%;
+//     display: flex;
+//     justify-content: flex-start;
+//     align-items: center;
+//     flex-direction: column;
+//     gap: 30px;
+// }
+
+// .hob-desc p {
+//     color: white;
+//     text-align: center;
+// }
+
+// .hob-desc img {
+//     width: max-content;
+//     height: 30vw;
+//     object-fit: cover;
+//     object-position: center;
+//     border-radius: 10px;
+// }
+
+// #hobby .head {
+//     width: max-content;
+//     color: white;
+//     border-bottom: 5px solid white;
+// }
+
+// #explore {
+//     width: 100%;
+//     height: max-content;
+//     padding: 16px;
+//     display: flex;
+//     justify-content: flex-start;
+//     align-items: center;
+//     flex-direction: column;
+//     gap: 20px;
+// }
+
+// #explore .head {
+//     width: max-content;
+// }
+
+// .text {
+//     padding: 20px;
+//     display: flex;
+//     justify-content: flex-start;
+//     align-items: center;
+//     flex-direction: column;
+//     gap: 20px;
+// }
+
+// @media screen and (max-width:800px) {
+//     .navbar h1 {
+//         font-size: 25px;
+//     }
+
+//     .navbar ul li a {
+//         font-size: 14px;
+//     }
+
+//     .navbar ul {
+//         gap: 10px;
+
+//     }
+
+//     .cta button {
+//         width: 6rem;
+//         height: 2.3rem;
+
+//     }
+
+//     #about-me {
+//         width: 100vw;
+//         height: 100vh;
+//         padding: 16px;
+//         display: flex;
+//         justify-content: flex-start;
+//         align-items: center;
+//         flex-direction: column;
+//         gap: 20px;
+//     }
+
+//     .head {
+//         text-align: center;
+//         font-size: 40px;
+//         font-weight: 800;
+
+//         font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+//         border-bottom: 5px solid #040E27;
+//     }
+
+//     #about-me div {
+//         width: 100%;
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//         flex-direction: row-reverse;
+//         padding: 20px;
+//         gap: 20px;
+//     }
+
+//     #about-me div img {
+//         width: 350px;
+//         height: max-content;
+//         object-fit: cover;
+//         object-position: center;
+//         border-radius: 10px;
+//     }
+
+//     #about-me div p {
+//         width: 700px;
+//         text-align: center;
+//     }
+
+//     footer {
+//         width: 100%;
+//         display: flex;
+//         justify-content: space-between;
+//         align-items: center;
+//         padding: 16px;
+//         background-color: #040E27;
+//     }
+
+//     footer p {
+//         font-size: 20px;
+//         color: white;
+//     }
+
+//     #hobby {
+//         padding: 8px;
+//         gap: 10px;
+//     }
+
+//     #about-me {
+//         padding: 8px;
+//         height: max-content;
+//     }
+
+//     #about-me div {
+//         flex-direction: column-reverse;
+//     }
+
+//     #about-me div p {
+//         padding: 10px;
+//         width: 100%;
+//     }
+// }
